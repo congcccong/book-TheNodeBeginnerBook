40381584f0e60545d9699e4a603c122da2887104
congcccong <302666299@qq.com>
2018-12-10 10:46:05 +0800

    blocking unsolved

---
+++ requestHandlers.js
@@ -0,0 +1,25 @@
+var exec = require('child_process').exec
+
+function start(response) {
+    console.log('Request handler "start" was called.')
+ 
+    // 休眠10秒
+    function sleep(milliSeconds) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + milliSeconds) {}
+    }
+    sleep(10000)
+
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write('Hello Start')
+    response.end()
+
+}
+function upload(response) {
+    console.log('Request handler "upload" was called')
+        response.writeHead(200, {'Content-Type': 'text/plain'})
+        response.write('Hello Upload')
+        response.end()
+}
+exports.start = start
+exports.upload = upload
